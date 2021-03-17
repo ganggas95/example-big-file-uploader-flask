d52af4e59ab65a059b02298d4fe96936782cc75b
ganggas95 <subhannizar25@gmail.com>
2021-03-18 05:39:24 +0800

    Fixing wrong algorithem

--- uploader/__init__.py
+++ uploader/__init__.py
@@ -1,7 +1,8 @@
 import os
 import json
-from flask import Flask
+from flask import Flask, abort
 from flask import request, render_template
+from flask_cors import CORS
 from werkzeug.utils import secure_filename
 
 
@@ -19,8 +20,11 @@ class BasicConfig(object):
 app_instance = Flask(
     __name__,
 )
+
+cors = CORS()
 app_instance.config.from_object(BasicConfig)
 
+cors.init_app(app_instance)
 FILES = dict({})
 
 
@@ -28,12 +32,14 @@ FILES = dict({})
 def upload():
     if request.method == "PUT":
         # Di sini akan diprosess file file yang sudah di split di Front-End
+        req_filename = request.args.get("filename", default=None, type=str)
+        if req_filename is None:
+            abort(400)
         files = request.files.get("file")
         # Selain File, dikirim juga meta data.
         # Alternative bisa menggunakan id saja
         # Bisa di taruh di query param atau query search
         # Di kasus ini saya taruh di form request
-        metadata = json.loads(request.form.get("metadata"))
 
         filename = secure_filename(files.filename)
 
@@ -45,25 +51,28 @@ def upload():
         # Baca size dari temporary file yang disimpan
         size = os.stat(full_path_temp_file).st_size
         # Menambahkan isi dari binary data dari temporary file
-        FILES[metadata["Name"]]["Data"] += files
+        print(FILES[req_filename]["data"])
+        with open(full_path_temp_file, 'rb') as file_temp:
+            FILES[req_filename]["data"].append(file_temp.read())
+            # file_temp.close()
         # Update besar file yang sudah di prosess
-        FILES[metadata["Name"]]["Downloaded"] += size
+        FILES[req_filename]["downloaded"] += size
         # Menampung nama file dari temprary file
-        FILES[metadata["Name"]]["FileTemp"].append(filename)
+        FILES[req_filename]["file_temp"].append(filename)
         # Jika File size sama dengan besar file yang sudah di prosess
-        if FILES[metadata["Name"]]["Size"] == FILES[metadata["Name"]]["Downloaded"]:
+        if FILES[req_filename]["size"] == FILES[req_filename]["downloaded"]:
             # Membuat Full path target file
             full_path_target_file = os.path.join(
                 app_instance.config.get("UPLOAD_FOLDER"),
-                metadata["Name"]
+                req_filename
             )
             # Buka dan buat file target / file hasil
-            with open(full_path_target_file, "+w") as file:
+            with open(full_path_target_file, "+wb") as file:
                 # Gabungkan binary files yang di simpan
                 # Di file target / file hasil dan simpan
-                file.writelines(FILES[metadata["Name"]]["Data"])
+                file.writelines(FILES[req_filename]["data"])
             # Menghapus temporary file yang sudah dibuat
-            for del_file in FILES[metadata["Name"]]["FileTemp"]:
+            for del_file in FILES[req_filename]["file_temp"]:
                 os.remove(os.path.join(
                     app_instance.config.get("TEMP_FOLDER"),
                     del_file
@@ -75,22 +84,22 @@ def upload():
             "status": "Uploading"
         }
     if request.method == 'POST':
-        # Di post hanya kirim meta data doang
+        # Di post hanya kirim meta data saja
         data = request.get_json(force=True)
         # Di bawah ini bisa disimpan di database
         # Ini untuk nama file
-        FILES[data["Name"]] = dict({"Name": data["Name"]})
+        FILES[data["filename"]] = dict({"filename": data["filename"]})
         # Ini untuk besar file
-        FILES[data["Name"]]["Size"] = data["Size"]
+        FILES[data["filename"]]["size"] = data["size"]
         # Ini untuk besar file yang sudah diprogress di BE
-        FILES[data["Name"]]["Downloaded"] = 0
+        FILES[data["filename"]]["downloaded"] = 0
         # Ini untuk menyimpan sementara binary data dari temporary file
-        FILES[data["Name"]]["Data"] = []
-        # Ini untuk menuimpan sementara nama file-file temporary
+        FILES[data["filename"]]["data"] = []
+        # Ini untuk menyimpan sementara nama file-file temporary
         # Tujuannya ketika selesai di gabungkan, file-file temporary akan dihapus
-        FILES[data["Name"]]["FileTemp"] = []
+        FILES[data["filename"]]["file_temp"] = []
         # Kirim meta data yang telah tersimpan
-        return FILES[data["Name"]]
+        return FILES[data["filename"]]
 
 
 @app_instance.route("/")
